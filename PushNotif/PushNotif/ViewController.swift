d09e2e86197624fe8fdafe445d16855a9bb903a1
Roli Bernanda <acappemenang@gmail.com>
2024-01-27 08:09:45 +0700

    feat: remote push notif

---
+++ PushNotif/PushNotif/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PushNotif
+//
+//  Created by Roli Bernanda on 27/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
